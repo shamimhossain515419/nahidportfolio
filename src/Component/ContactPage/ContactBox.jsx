f6a96247bcb10583d3f4242cbc46a58518a8f228
Shamimusman515419 <shamimhossain01617@gmail.com>
2023-09-05 21:26:33 +0600

    contact

--- src/Component/ContactPage/ContactBox.jsx
+++ src/Component/ContactPage/ContactBox.jsx
@@ -1,22 +1,23 @@
 'use client';
+
 import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 import toast from 'react-hot-toast';
 const ContactBox = () => {
-
+     const form = useRef();
      const sendEmail = (e) => {
           e.preventDefault();
           const from = e.target;
-          console.log(from.name.value);
-          // emailjs.sendForm('service_3aw79ro', 'template_141fz2g', form.current, 'YDQbnPcNUrXTXhLlP')
-          //      .then((result) => {
-          //           if (result.text) {
-          //                toast('Success Message')
-          //                from.reset();
-          //           }
+          emailjs.sendForm('service_bvjenph', 'template_sobq5nx', form.current, 'E3IxF18fQaZgMT0_Z')
+               .then((result) => {
+                    if (result.text) {
+                         toast('Success Message')
+                         from.reset();
+                    }
 
-          //      }, (error) => {
-          //           toast(`${error.message}`)
-          //      });
+               }, (error) => {
+                    toast(`${error.message}`)
+               });
      };
 
 
@@ -26,7 +27,7 @@ const ContactBox = () => {
           <div>
                <div className=" mt-10  md:max-w-[1000px] mx-auto">
 
-                    <form onSubmit={sendEmail}>
+                    <form ref={form} onSubmit={sendEmail}>
                          <div className=" w-full my-2">
                               <label htmlFor="Name" className=" text-xl font-semibold  mt-3" > Name:</label>
                               <input required className="mt-2 w-full  border border-[#100f0f]  focus:border-none hover:outline-1   focus:outline rounded-lg focus:outline-[#582ad8] p-2 text-lg font-semibold " type="text" placeholder=" Your name " name="name" id="" />
@@ -40,7 +41,7 @@ const ContactBox = () => {
                               <textarea required className="p-2 border border-[#100f0f]  focus:border-none     mt-2 w-full   hover:outline-1   rounded-lg focus:outline focus:outline-[#582ad8] " name="message" rows={8} ></textarea>
                          </div>
                          <div className=" w-full my-2">
-                              <button type="submit" className=" buttonBox  shadow-md  outline-none border-none text-xl font-medium py-2 rounded-md text-center w-full " >Send massage</button>
+                              <button type="submit" className=" contactButton  shadow-md  outline-none border-none text-xl font-medium py-2 rounded-md text-center w-full " >Send massage</button>
                          </div>
                     </form>
                </div>
--- src/Component/VisitingCard/VisitingCard.jsx
+++ src/Component/VisitingCard/VisitingCard.jsx
@@ -1,5 +1,9 @@
 import GetProduct from "@/Utili/GetProduct";
 import Image from "next/image";
+import Link from "next/link";
+import { BsEye } from "react-icons/bs";
+
+
 const VisitingCard = async () => {
      const product = await GetProduct();
      const projectData = product?.sort((a, b) => new Date(b.date) - new Date(a.date));
@@ -9,8 +13,8 @@ const VisitingCard = async () => {
                <div className='  md:mx-[150px] max-w[2400] 2xl:px-32 xl:px-24 md:px-16 px-3'>
                     <div  >
                          <div className=" text-center ">
-                              <h1 className=" text-xl text-[#1a53ff] md:text-2xl  xl:text-3xl  mt-4 font-bold"> Visiting Card Design</h1>
-                              <p className=" text-base font-semibold my-2  ">   latest Work</p>
+                              <h1 className=" text-xl text-[#1a53ff] md:text-2xl  capitalize xl:text-3xl  mt-4 font-bold"> latest Work</h1>
+
                          </div>
 
                          <div className=" my-11">
@@ -19,14 +23,29 @@ const VisitingCard = async () => {
                                    {
                                         projectData?.slice(0, 3).map(item =>
                                              <div key={item._id} className=" cursor-pointer relative  w-full   ">
-                                                  <Image width={500} height={500} className=" w-[500px]   relative object-contain  " src={item?.img} alt=""></Image>
-                                                  <div className=" top-0 duration-300  absolute hover:bg-[#00000093] w-full h-full ">
+                                                  <Link href={'/work'}>
+                                                       <Image width={500} height={500} className=" w-[500px]   relative object-contain  " src={item?.img} alt=""></Image>
+                                                       <div className=" top-0 duration-300  absolute hover:bg-[#00000093] w-full h-full ">
 
-                                                  </div>
+                                                       </div>
+                                                  </Link>
                                              </div>)
                                    }
 
 
+                              </div>
+
+                              <div className=" flex mt-10  justify-center items-center gap-2">
+
+                                   <div className=" inline-block ">
+                                        <Link
+                                             href="/work"
+                                             className=" flex buttonBox my-6  text-lg boxshadow items-center gap-2"
+                                        >
+                                             <span>See my all work</span>
+                                             <BsEye size={24}></BsEye>
+                                        </Link>
+                                   </div>
                               </div>
                          </div>
                     </div>
